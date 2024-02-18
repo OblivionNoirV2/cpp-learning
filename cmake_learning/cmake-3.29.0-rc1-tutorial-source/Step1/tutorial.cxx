661b2760d013e00e417d233bdd93efa03038331a
OblivionNoirV2 <roslyndo79@gmail.com>
2024-02-17 22:02:54 -0600

    cmake is hell

--- cmake_learning/cmake-3.29.0-rc1-tutorial-source/Step1/tutorial.cxx
+++ cmake_learning/cmake-3.29.0-rc1-tutorial-source/Step1/tutorial.cxx
@@ -6,18 +6,18 @@
 
 // TODO 11: Include TutorialConfig.h
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[]) //number of command line arguments given, array of strings 
 {
-  if (argc < 2) {
+  if (argc < 2) { //ensures there is at least 2 argument given (including the project name). if not, return 1
     // TODO 12: Create a print statement using Tutorial_VERSION_MAJOR
     //          and Tutorial_VERSION_MINOR
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    std::cout << "Usage: " << argv[0] << " number" << std::endl; //project name is always passed by the os, so index 0 always exists 
     return 1;
   }
 
   // convert input to double
   // TODO 4: Replace atof(argv[1]) with std::stod(argv[1])
-  const double inputValue = atof(argv[1]);
+  const double inputValue = atof(argv[1]); //atof converts to double 
 
   // calculate square root
   const double outputValue = sqrt(inputValue);
