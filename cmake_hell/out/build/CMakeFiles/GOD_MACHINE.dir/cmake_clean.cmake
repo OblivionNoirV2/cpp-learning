8fca5d21e9ff69cc83b2f26a2756c4ac58122246
OblivionNoirV2 <roslyndo79@gmail.com>
2024-02-25 15:06:32 -0600

    backup 7

---
+++ cmake_hell/out/build/CMakeFiles/GOD_MACHINE.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GOD_MACHINE.dir/main.cpp.o"
+  "CMakeFiles/GOD_MACHINE.dir/main.cpp.o.d"
+  "GOD_MACHINE"
+  "GOD_MACHINE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GOD_MACHINE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
