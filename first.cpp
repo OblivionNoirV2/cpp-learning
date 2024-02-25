c100cac49946400efdbfff6a0e9592aef006355c
OblivionNoirV2 <roslyndo79@gmail.com>
2024-02-25 14:28:36 -0600

    fucked up, trying again

--- first.cpp
+++ first.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
-#include <cstdint> 
 
-int main() {
-    int64_t x = 64;
-
-    std::cout << "Address of x: " << &x << std::endl;
-    std::cout << "Working! Signed x: " << x <<std::endl;
-    std::cout << "test2" <<std::endl;
-
+using namespace std;
+int main(){
+    cout << "hello" << endl; 
     return 0;
 }
